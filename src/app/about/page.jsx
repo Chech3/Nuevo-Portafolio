81221862fc39ecb970f66dd3153f10544fb37ebc
Chech3 <joseangelgarces123@gmail.com>
2024-07-22 13:43:02 -0400

    Agregado los popup

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,10 @@
 'use client'
 import React from 'react'
 import { motion } from "framer-motion"
-import TecIcons from '../components/TecIcons'
+import dynamic from 'next/dynamic'
+const TecIcons = dynamic(() => import('../components/TecIcons'), {
+  ssr: false,
+});
 
 function AboutPage() {
   return (
@@ -40,10 +43,8 @@ function AboutPage() {
 
             </div>
             {/* BIOGRAPHY SCROLL SVG */}
-            <motion.svg
-              initial={{ opacity: 0.2, y: 0 }}
-              animate={{ opacity: 1, y: "10px" }}
-              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+            <svg
+              className='motion-safe:animate-bounce'
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +62,7 @@ function AboutPage() {
                 stroke="#000000"
                 strokeWidth="1"
               ></path>
-            </motion.svg>
+            </svg>
           </div>
           {/* SKILLS CONTAINER */}
           <div className='flex flex-col gap-12 justify-center'>
@@ -69,6 +70,28 @@ function AboutPage() {
             <TecIcons />
 
           </div>
+
+          <svg
+              className='motion-safe:animate-bounce'
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </svg>
+
           {/* EXPERIENCIA CONTAINER */}
           <div className='flex flex-col gap-12 justify-center'>
             <h2 className='font-bold dark:text-white text-2xl'>EXPERIENCE</h2>
